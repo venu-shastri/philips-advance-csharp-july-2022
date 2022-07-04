10db6d273eda6f895bae4d51767d744ef7ca001b
Venugopal  K (C) <vgk@tritechsamvit.com>
2022-07-04 12:22:42 +0530

    source Code updated

---
+++ App/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal delegate bool FilterCommand(string item);
+    interface IFilterStrategy
+    {
+        bool Predicate(string item);
+
+    }
+
+    //SRP
+    class CheckStringStartsWithAny : IFilterStrategy
+    {
+        public string StartsWith { get; set; }
+       public  bool Predicate(string item)
+        {
+            return item.StartsWith(StartsWith);
+        }
+
+
+    }
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Philips", "Siemens", "Cerner", "Apple", "Oracle" };
+            //names.Where(s => s.StartsWith("s")); // > 3.0 
+            CheckStringStartsWithAny strategy = new CheckStringStartsWithAny() { StartsWith="S"};
+           List<string> result= Filter(names,strategy);
+            foreach(string item in result)
+            {
+                Console.WriteLine(item);    
+            }
+            strategy.StartsWith = "P";
+
+            FilterCommand _filterCommand = new FilterCommand(strategy.Predicate);
+           result= Filter(names, _filterCommand);
+            
+            foreach (string item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+        static List<string> Filter(string[] source,IFilterStrategy logic )
+        {
+            //find name from names where name endswith "s"
+            List<string> resultAggregator = new List<string>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (logic.Predicate(source[i]))
+                {
+                    resultAggregator.Add(source[i]);
+                }
+            }
+            return resultAggregator;
+        }
+
+        static List<string> Filter(string[] source, FilterCommand logic)
+        {
+            //find name from names where name endswith "s"
+            List<string> resultAggregator = new List<string>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                //Invke Command
+                if (logic.Invoke(source[i]))
+                {
+                    resultAggregator.Add(source[i]);
+                }
+            }
+            return resultAggregator;
+        }
+
+
+
+    }
+}
