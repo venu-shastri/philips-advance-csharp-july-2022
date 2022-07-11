ae7a75c0b1bd3a888868e51e55a4804528755dad
Venugopal  K (C) <vgk@tritechsamvit.com>
2022-07-11 12:53:59 +0530

    Provider Demo

---
+++ ProviderDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //select all the patients where location="blr"
+        }
+    }
+}
