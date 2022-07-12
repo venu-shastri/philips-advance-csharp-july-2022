53bb16e7008b0c6f7d8e2f815e1b887265018816
Venugopal  K (C) <vgk@tritechsamvit.com>
2022-07-12 13:24:16 +0530

    Parallel updated

--- PerformanceCode/Program.cs
+++ PerformanceCode/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Collections.Concurrent;
 
 namespace PerformanceCode
 {
@@ -11,22 +12,43 @@ namespace PerformanceCode
     {
         static void Main(string[] args)
         {
+            //List<string> encryptedContent = new List<string>();
+            ConcurrentBag<string> _threadSafeList = new ConcurrentBag<string>();
+            Console.WriteLine(System.Environment.ProcessorCount);
+            string[] content = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k" };
+            //content.AsParallel().WithDegreeOfParallelism(2).Select(item => Encrypt(item)).ForAll((item) => {
+            //Console.WriteLine($"{System.Threading.Thread.CurrentThread.ManagedThreadId}->Item {item}");
+            //});
+            Partitioner.Create(content, true).
+                AsParallel().
+                WithDegreeOfParallelism(2).
+                Select(item => Encrypt(item)).
+                ForAll((item) => {
+                        Console.WriteLine($"{System.Threading.Thread.CurrentThread.ManagedThreadId}->Item {item}");
+                        _threadSafeList.Add(item);
+                   
+                });
+
+            }
+
+        static void ParallelDemo()
+        {
             string[] content = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k" };
             System.Diagnostics.Stopwatch watcher = new System.Diagnostics.Stopwatch();
-            watcher.Start();
-            var enryptedContent=content.Select(item => Encrypt(item)).ToList();//Eager
+           // watcher.Start();
+            var enryptedContent=content.AsParallel().Select(item => Encrypt(item)).ToList();//Eager
             watcher.Stop();
-            Console.WriteLine("Elapsed Miliseconds "+watcher.ElapsedMilliseconds);
-            foreach(var item in enryptedContent)
+            Console.WriteLine("Elapsed Miliseconds " + watcher.ElapsedMilliseconds);
+            foreach (var item in enryptedContent)
             {
                 Console.WriteLine(item);
             }
-
         }
 
         static string Encrypt(string item)
         {
             Thread.Sleep(2000);
+            if (item == "b") { Thread.Sleep(15000); }
             return item.ToUpper();
         }
     }
