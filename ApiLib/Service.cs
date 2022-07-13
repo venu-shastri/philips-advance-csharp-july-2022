a2f0942145d10e590c427ab9bc21575c67d2e379
Venugopal  K (C) <vgk@tritechsamvit.com>
2022-07-13 12:17:12 +0530

    Update

---
+++ ApiLib/Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiLib
+{
+    //[Serializable]
+    public class Service:System.MarshalByRefObject,ApiContractLib.IService
+    {
+        public Service()
+        {
+          Console.WriteLine($"Service Instantiated in {System.AppDomain.CurrentDomain.FriendlyName} Domain");  
+
+        }
+        public void Operation()
+        {
+
+            Console.WriteLine($"Service Opration Executed in {System.AppDomain.CurrentDomain.FriendlyName} Domain");
+        }
+
+    }
+}
---
+++ DebuggerApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //while(true)
+            //{
+            //    Console.WriteLine("Hello! From Main");
+            //    System.Threading.Thread.Sleep(1000);
+
+            //}
+            Console.WriteLine("Set First Breakpoint");
+           // Console.ReadLine();
+            Client();
+        }
+        static void Client()
+        {
+            Console.WriteLine($"Client Code Executed in {System.AppDomain.CurrentDomain.FriendlyName}");
+            AppDomain _isolatedDomain=AppDomain.CreateDomain("Isolated");
+          var handle=  _isolatedDomain.CreateInstance("ApiLib", "ApiLib.Service");
+            Console.WriteLine("set Second Breakpoint");
+           // Console.ReadLine();
+          object _objectRef=  handle.Unwrap() ;
+            //ApiLib.Service _instance = new ApiLib.Service();
+            //Console.WriteLine("Set Second Breakpoint");
+            //Console.ReadLine();
+           ApiContractLib.IService _instance = _objectRef as ApiContractLib.IService;
+            _instance.Operation();
+
+            Console.WriteLine("Its Time to Take Dump");
+            Console.ReadLine();
+
+        }
+    }
+}
